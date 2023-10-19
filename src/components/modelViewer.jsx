c1d772be735a75d2a6e13084659d4a7680327753
ziadk432 <ziadk432@hotmail.com>
2023-10-20 02:56:05 +0300

    updatess

--- src/components/modelViewer.jsx
+++ src/components/modelViewer.jsx
@@ -15,7 +15,7 @@ export function ModelViewer({ modelSrc, modelImg, cameraPosition, modelPos }) {
     const [isVisibleModel, setIsVisibleModel] = React.useState(false);
     const [isAnimating, setIsAnimating] = React.useState(false);
     const [isTexture, setIsTexture] = React.useState(modelSrc);
-    const [isIntensity, setIsIntensity] = React.useState(1);
+    const [isIntensity, setIsIntensity] = React.useState(4);
 
 
     function changeTex1() {
@@ -68,7 +68,7 @@ export function ModelViewer({ modelSrc, modelImg, cameraPosition, modelPos }) {
                                 fov: 35,
                                 position: cameraPosition,
                             }}
-                            style={{ width: '100%', height: '90%' }}
+                            style={{ width: '100%', height: '80%' }}
                         >
                             <OrbitControls makeDefault />
 
@@ -93,42 +93,80 @@ export function ModelViewer({ modelSrc, modelImg, cameraPosition, modelPos }) {
 
                     <Link rel="stylesheet" href="https://unpkg.com/@leoncvlt/ar-button/styles.css" />
                     <ar-button
-                        src="https://firrnas-models.s3.eu-west-3.amazonaws.com/elAraby/Washing+Machine+Black+Huss.glb"
-                        ios-src="https://firrnas-models.s3.eu-west-3.amazonaws.com/elAraby/2+scenes+1+washing+machine+.reality"
-                        link="https://www.nasa.gov/"
-                        title="A 3D model of an astronaut">
-                        See in Augmented Reality
+                        src="/Washing Machine + Animation Black.glb"
+                        ios-src="https://firrnas-models.s3.eu-west-3.amazonaws.com/elAraby/black+washing+.reality"
+                        title="Black washing machine">
+                        Black IOS & Android AR
                     </ar-button>
+
+                    <ar-button
+                        src="/Washing Machine + Animation Black.glb"
+                        ios-src="https://firrnas-models.s3.eu-west-3.amazonaws.com/elAraby/2+scenes+1+washing+machine+.reality"
+                        title="Silver washing machine">
+                        Silver IOS & Android AR
+                    </ar-button>
+
+                    <ar-button
+                        src=""
+                        ios-src="https://firrnas-models.s3.eu-west-3.amazonaws.com/elAraby/2+scenes+1+washing+machine+final+2.reality"
+                        title="Compare Silver & black washing machine">
+                        Compare Silver & Black IOS AR
+                    </ar-button>
+
+                    <ar-button
+                        src="Washing Machine White00.glb"
+                        ios-src=""
+                        title="Compare Silver & black washing machine">
+                        White Android AR
+                    </ar-button>
+
                     <Script type="module" src="https://unpkg.com/@leoncvlt/ar-button"></Script>
 
 
 
                 </div>}
-                {isVisibleModel && <div className="d-flex justify-content-center">
-                    <button onClick={changeTex1} className="btn btn-primary mx-1">
-                        Black
-                    </button>
-                    <button onClick={changeTex2} className="btn btn-primary mx-1">
-                        Silver
-                    </button>
-                    <button onClick={changeTex3} className="btn btn-primary mx-1">
-                        White
-                    </button>
-                    <button onClick={anime} className="btn btn-primary mx-1">
-                        Play animation
-                    </button>
-                    {platform.os.family == "iOS" && <button className="btn btn-outline-dark mx-1">
-                        <a rel="ar" href="2 scenes 1 washing machine .reality" style={{ textDecoration: 'none' }}>
-                            View in AR for iOS
-                        </a>
-                    </button>}
-                    {platform.os.family == "Android" &&
-                        <button className="btn btn-outline-dark mx-1">
-                            <a className="" theme="dark" data-ar="true" rel="ar" href='intent://arvr.google.com/scene-viewer/1.0?file=/WashingMachineBlackHuss.glb#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;' style={{ textDecoration: 'none' }}>
-                                View in AR for Android
+                {isVisibleModel && <div className="container-fluid" >
+
+                    <div class="d-flex flex-column mb-3  justify-content-center">
+                        <button onClick={changeTex1} className="btn btn-primary mx-1 my-1 align-self-center" style={{
+                            width: '150px'
+                        }}>
+                            Black
+                        </button>
+                        <button onClick={changeTex2} className="btn btn-primary mx-1 my-1 align-self-center" style={{
+                            width: '150px'
+                        }}>
+                            Silver
+                        </button>
+                        <button onClick={changeTex3} className="btn btn-primary mx-1 my-1 align-self-center" style={{
+                            width: '150px'
+                        }}>
+                            White
+                        </button>
+                        <button onClick={anime} className="btn btn-primary mx-1 my-1  align-self-center" style={{
+                            width: '150px'
+                        }}>
+                            Play animation
+                        </button>
+                    </div>
+
+
+
+
+
+                    {/* {platform.os.family == "iOS" && <button className="btn btn-outline-dark mx-1">
+                            <a rel="ar" href="2 scenes 1 washing machine .reality" style={{ textDecoration: 'none' }}>
+                                View in AR for iOS
                             </a>
                         </button>}
-                </div>}
+                        {platform.os.family == "Android" &&
+                            <button className="btn btn-outline-dark mx-1">
+                                <a className="" theme="dark" data-ar="true" rel="ar" href='intent://arvr.google.com/scene-viewer/1.0?file=/WashingMachineBlackHuss.glb#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;S.browser_fallback_url=https://developers.google.com/ar;end;' style={{ textDecoration: 'none' }}>
+                                    View in AR for Android
+                                </a>
+                            </button>} */}
+                </div>
+                }
 
             </div >
         </>
