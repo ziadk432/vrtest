b25eda723fb55391ba3dd798b334e682be74b686
ziadk432 <ziadk432@hotmail.com>
2023-10-19 07:39:54 +0300

    model image

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,12 +7,12 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <>
-        <div className='d-flex justify-content-center' style={{
+        <div className='d-flex justify-content-center align-items-center' style={{
           width: '100%',
           height: '100vh',
           backgroundRepeat: 'no-repeat'
         }}>
-          <ModelViewer className='gx-5 px-5' modelImg={"https://firrnas-models.s3.eu-west-3.amazonaws.com/pix/chair-removebg-preview.png"} modelSrc={"/Washing Machine White00.glb"} cameraPosition={[0, 1, 3]} modelPos={[0, -0.5, 0]}></ModelViewer>
+          <ModelViewer className='gx-5 px-5' modelImg={"/tl_slbs_9.0kg_fv.webp"} modelSrc={"/Washing Machine White00.glb"} cameraPosition={[0, 1, 3]} modelPos={[0, -0.5, 0]}></ModelViewer>
         </div>
       </>
 
